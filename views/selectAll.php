86e14288c6e71715f43d06fe7d2b1f6c161743a2
tonyal128 <anthony.r.larson@gmail.com>
2018-04-28 20:33:16 -0500

    Show all data in table in html table

---
+++ views/selectAll.php
@@ -0,0 +1,32 @@
+<?php
+	
+    $conn = require("../models/connectionSkills.php");		//connects to the database
+    
+    try{
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+	$stmt = $conn->prepare("SELECT event_id,event_name,event_description FROM wdv341_event");
+	$stmt->execute();
+?>
+<table border='1'>
+	<tr>
+		<td>ID</td>
+		<td>Name</td>
+		<td>Description</td>
+<?php 
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+	{
+		echo "<tr>";
+			echo "<td>" . $row['event_id'] . "</td>";
+			echo "<td>" . $row['event_name'] . "</td>";	
+			echo "<td>" . $row['event_description'] . "</td>";	
+		echo "</tr>";
+	}
+?>
+</table>
